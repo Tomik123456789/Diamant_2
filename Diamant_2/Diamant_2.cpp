3fbee2c8e7dd1c96372c99cf92ec240fb7000e07
Uzivatel <Uzivatel@POTA>
2022-01-26 15:08:30 +0100

    Přidejte soubory projektu.

---
+++ Diamant_2/Diamant_2.cpp
@@ -0,0 +1,19 @@
+﻿
+
+#include <stdio.h>
+
+int main()
+{
+	for (int j = 1; j <= 9; j++) {
+		for (int i = 1; i <= j; i++) {
+			printf("%3d", i);
+		}
+		printf("\n");
+	}
+	for (int j = 8; j >= 2; j--) {
+		for (int i = 1; i <= j; i++) {
+			printf("%3d", i);
+		}
+		printf("\n");
+	}
+}
